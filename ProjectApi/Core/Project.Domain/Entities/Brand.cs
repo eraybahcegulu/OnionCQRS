20790bda5bc0bb005aa72b31bc7eac95643ecf34
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-03-18 23:35:42 +0300

    Entities, Configurations, dbcontext, Bogus

---
+++ ProjectApi/Core/Project.Domain/Entities/Brand.cs
@@ -0,0 +1,21 @@
+﻿using Project.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        public Brand()
+        {
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
