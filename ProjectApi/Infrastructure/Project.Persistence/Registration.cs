01e427ac458b2a6bd67168b431af0a4d494d0f16
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-03-19 00:38:14 +0300

    units of work

--- ProjectApi/Infrastructure/Project.Persistence/Registration.cs
+++ ProjectApi/Infrastructure/Project.Persistence/Registration.cs
@@ -2,8 +2,10 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Project.Application.Interfaces.Repositories;
+using Project.Application.Interfaces.UnitOfWorks;
 using Project.Persistence.Context;
 using Project.Persistence.Repositories;
+using Project.Persistence.UnitOfWorks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +23,8 @@ namespace Project.Persistence
 
             services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
             services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
     }
 }
