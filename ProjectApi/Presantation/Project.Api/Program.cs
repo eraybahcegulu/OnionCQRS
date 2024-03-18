2a4223cb2ed61e51966b76b751da61a0d6ca8e83
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-03-19 01:12:33 +0300

    Products queries

--- ProjectApi/Presantation/Project.Api/Program.cs
+++ ProjectApi/Presantation/Project.Api/Program.cs
@@ -1,4 +1,5 @@
 using Project.Persistence;
+using Project.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,7 @@ builder.Configuration
     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
