023e3a7fb302a5b46633b543eca778e3925058be
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-03-21 04:20:51 +0300

    Create NotAuth.tsx

---
+++ client/src/pages/NotAuth.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@nextui-org/react'
+import { Result } from 'antd'
+import { useNavigate } from 'react-router-dom';
+
+const NotAuth = () => {
+    const navigate = useNavigate();
+    return (
+        <div className="min-h-screen max-h-full bg-gradient-to-b  p-20 flex justify-center items-center ">
+            <Result
+                status="403"
+                title="403"
+                subTitle="Sorry, you are not authorized to access this page."
+                extra={<Button onClick={() => navigate('/')} color='primary'>Back Home</Button>}
+            />
+        </div>
+    )
+}
+
+export default NotAuth
